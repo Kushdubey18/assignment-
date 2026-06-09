93f4bc96c341cf386052c1a2978f00fbe7d55010
kush Dubey <166368929+Kushdubey18@users.noreply.github.com>
2026-06-09 10:28:44 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,40 @@
+# q1...........
+
+name = "Kush"
+
+
+print("First character:", name[0])
+print("Last character:", name[-1])
+print("Length of string:", len(name))
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
+print("Reversed string:", name[::-1])
+
+# q 2...............
+
+
+text = "PythonProgramming"
+print("First four characters:", text[:4])
+print("Characters from index 2 to 5:", text[2:6])
+print("Reverse string:", text[::-1])
+
+
+
+#  q3..........
+
+numbers = [10, 20, 30, 40, 20]
+
+numbers.append(50)
+print("After append(50):", numbers)
+numbers.insert(2, 25)   
+print("After insert(2, 25):", numbers)
+numbers.remove(20)      
+print("After remove(20):", numbers)
+numbers.pop()
+print("After pop():", numbers)
+numbers.reverse()
+print("After reverse():", numbers)
+numbers.sort()
+print("After sort():", numbers)
+print("Length of the list:", len(numbers))
+print("Occurrence of 20:", numbers.count(20))
