57cad51b4aae085cd0edf5d6bff29e3315fa4549
kush Dubey <166368929+Kushdubey18@users.noreply.github.com>
2026-06-09 17:59:00 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -51,4 +51,108 @@ print(max(tpl))
 print(min(tpl))
 print(sum(tpl))
 
+#q5...........
+
+# Tuple Packing and Unpacking
+
+t = (10, 20, 30, 40)   # Packing
+
+a, b, c, d = t         # Unpacking
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+
+#q6...........
+
+
+# Student Details Dictionary
+
+student = {
+    "Name": "Kush",
+    "Age": 20,
+    "Course": "B.Tech",
+    "Address": "Jaipur"
+}
+
+#  all keys
+print("Keys:", student.keys())
+
+#  all values
+print("Values:", student.values())
+
+#  all items
+print("Items:", student.items())
+
+#  Address
+student["Address"] = "Patna"
+
+# Add new key Branch
+student["Branch"] = "CSE"
+
+print("\nUpdated Dictionary:")
+print(student)
+
+
+#q7.............
+
+
+lst = [1, 2, 3, 4, [2, 5], 7]
+
+print(lst[4][1])
+
+#q8..............
+
+
+num = int(input("Enter a number: "))
+
+num += 10
+
+print("Updated value:", num)
+
+#q9..........
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Multiplication =", a * b)
+
+
+#q10..........
+
+
+student = {
+    "Name": "Kush",
+    "Age": 20,
+    "Course": "B.Tech"
+}
+
+print(student.get("Name"))   
+print(student.keys())        
+print(student.values())      
+print(student.items())      
+
+
+#q11..........
+
+
+list1 = [10, 20, 30, 40, 50]
+
+list2 = list1.copy()
+
+print("Original List:", list1)
+print("Copied List:", list2)
+
+
+
+
+
+
+
+
+
+
 
